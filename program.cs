f4a8ae0af4ea165e251ebde792eed639ddb9b306
brayanperez831-ctrl <brayan.perez831@pascualbravo.edu.co>
2026-05-25 23:25:26 -0500

    metodos para calculos

--- program.cs
+++ program.cs
@@ -10,3 +10,17 @@ namespace HotelReservas
         }
     }
 }
+using System;
+
+namespace HotelReservas
+{
+    internal class Program
+    {
+        static void Main(string[] args) { }
+
+        // Métodos de cálculo
+        static double CalcularSubtotal(double precioNoche, int noches) => precioNoche * noches;
+        static double CalcularDescuento(double subtotal, int noches) => (noches >= 5) ? subtotal * 0.15 : 0;
+        static double CalcularImpuesto(double subtotal) => subtotal * 0.10;
+    }
+}
