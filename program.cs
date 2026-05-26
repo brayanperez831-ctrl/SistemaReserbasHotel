45decf975e4a5c851eb7a5ea6a4644368c1f5b66
brayanperez831-ctrl <brayan.perez831@pascualbravo.edu.co>
2026-05-25 23:27:15 -0500

    tryparse para metodos mas robustos

--- program.cs
+++ program.cs
@@ -24,3 +24,23 @@ namespace HotelReservas
         static double CalcularImpuesto(double subtotal) => subtotal * 0.10;
     }
 }
+using System;
+
+namespace HotelReservas
+{
+    internal class Program
+    {
+        static void Main(string[] args) { }
+
+        static int LeerOpcion()
+        {
+            int opcion;
+            do { Console.Write("\nSeleccione: "); } 
+            while (!int.TryParse(Console.ReadLine(), out opcion));
+            return opcion;
+        }
+
+        static double CalcularSubtotal(double precioNoche, int noches) => precioNoche * n;
+        static double CalcularDescuento(double subtotal, int noches) => (noches >= 5) ? subtotal * 0.15 : 0;
+    }
+}
