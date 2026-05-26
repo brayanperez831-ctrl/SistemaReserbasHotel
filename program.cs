3654c494631ef6beb6455196022b98a8ae89b531
brayanperez831-ctrl <brayan.perez831@pascualbravo.edu.co>
2026-05-25 23:24:12 -0500

    estructura inicial de el proyecto

---
+++ program.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace HotelReservas
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Estructura inicial del sistema de reservas
+        }
+    }
+}
